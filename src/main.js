5075b2ed589fb9a253c4d9f47ad60fb64cc53471
Sara <sarailic431@gmail.com>
2025-03-20 14:58:32 +0100

    js

--- src/main.js
+++ src/main.js
@@ -5,5 +5,34 @@ const likeBtn = document.querySelector(".like-icon");
 
 likeBtn.addEventListener("click", function (e) {
   console.log("like icon is clicked");
-  e.target.style.color = "green";
+  e.target.style.color = "blue";
 });
+
+// prettier-ignore
+const friends = ["Angelina Simonovska", "Areous Ahmad", "Carol Hamada", "Jorge Fakhouri", "Julie Thompson", "Nina Vuksanovic"];
+
+class Account {
+  constructor(name) {
+    this.name = name;
+    // prettier-ignore
+    this.friends = ["Angelina Simonovska", "Areous Ahmad", "Carol Hamada", "Jorge Fakhouri", "Julie Thompson", "Nina Vuksanovic"];
+    this.friendsNumber = friends.length;
+    // this.posts = [];
+    // this.likes = [];
+  }
+}
+
+class Post {
+  constructor() {
+    this.postText = "";
+    this.likes = [];
+    this.personCommented = [];
+  }
+}
+
+class Comment {
+  constructor() {
+    this.commentText = "";
+    this.commentId = crypto.randomUUID();
+  }
+}
