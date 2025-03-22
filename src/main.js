25a555dd196b417d1245cfd30ff2caf3e5462603
Sara <sarailic431@gmail.com>
2025-03-22 13:53:13 +0100

    rendering friends photos

--- src/main.js
+++ src/main.js
@@ -8,9 +8,20 @@ likeBtn.addEventListener("click", function (e) {
   e.target.style.color = "blue";
 });
 
+const friendsListEl = document.querySelector(".friends-list");
+
+const friends = [
+  { name: "Angelina Simonovska", photo: "/angelina-simonovska.webp" },
+  { name: "Marc Anderson", photo: "/marc-anderson.webp" },
+  { name: "Carol Hamada", photo: "/carol-hamada.jpeg" },
+  { name: "Jorge Fakhouri", photo: "/jorge-fakhouri.webp" },
+  { name: "Julie Thompson", photo: "/julie-thompson.webp" },
+  { name: "Nina Vuksanovic", photo: "/nina-vuksanovic.jpeg" },
+];
+
 class Account {
-  constructor(name) {
-    this.name = name;
+  constructor() {
+    this.name = "Sara";
     this.friends = [];
     this.posts = [];
   }
@@ -23,24 +34,61 @@ class Account {
     return this.friends.length;
   }
 }
-
 const account = new Account();
-account.addFriend("Angelina Simonovska");
-account.addFriend("Areous Ahmad");
-account.addFriend("Carol Hamada");
-account.addFriend("Jorge Fakhouri");
-account.addFriend("Julie Thompson");
-account.addFriend("Nina Vuksanovic");
 
+class Friend {
+  constructor() {
+    // this.names = [];
+    // this.photos = [];
+
+    this.friends = [];
+  }
+
+  // addFriend(friend) {
+  //   this.names.push(friend);
+  // }
+
+  // addPhoto(photo) {
+  //   this.photos.push(photo);
+  // }
+
+  addFriend(name, photo) {
+    this.friends.push({ name: name, photo: photo });
+  }
+}
+
+const friend = new Friend();
+
+friends.forEach(function (person) {
+  account.addFriend(person.name);
+
+  friend.addFriend(person.name, person.photo);
+});
+
+console.log(friend);
 console.log(account.friends);
-console.log(account.getFriendsNumber());
+
+friend.friends.forEach(function (person) {
+  const friendEl = document.createElement("li");
+
+  // prettier-ignore
+  friendEl.innerHTML = ` <img class="img-friend" src=${person.photo} alt="profile" />
+            <p class="friend-name">${person.name}</p>`;
+  friendEl.className = "transaction-item";
+  friendsListEl.appendChild(friendEl);
+});
 
 class Post {
   constructor() {
     this.postText = "";
     this.likes = [];
     this.comments = [];
+    this.id = crypto.randomUUID();
   }
+}
+
+class Like {
+  constructor() {}
 }
 
 // napravi clasu like
@@ -52,3 +100,14 @@ class Comment {
     this.commentId = crypto.randomUUID();
   }
 }
+
+// const account = new Account();
+// account.addFriend("Angelina Simonovska");
+// account.addFriend("Marc Anderson");
+// account.addFriend("Carol Hamada");
+// account.addFriend("Jorge Fakhouri");
+// account.addFriend("Julie Thompson");
+// account.addFriend("Nina Vuksanovic");
+
+// console.log(account.friends);
+// console.log(account.getFriendsNumber());
