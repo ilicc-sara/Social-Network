139d46c5be9f2ab7ecc249f59f903e3a8df517ba
Sara <sarailic431@gmail.com>
2025-03-24 20:28:56 +0100

    added post element and adding comments and likes

--- src/main.js
+++ src/main.js
@@ -78,9 +78,17 @@ class Post {
     this.name = name;
     this.postText = postText;
     this.postDate = date;
+    this.id = crypto.randomUUID();
     this.likes = [];
     this.comments = [];
-    this.id = crypto.randomUUID();
+  }
+
+  addLikes(perosn) {
+    this.likes.push(perosn);
+  }
+
+  addComment(person, commentText) {
+    this.comments.push({ person: person, commentText: commentText });
   }
 }
 
@@ -94,47 +102,48 @@ posts.forEach(function (postItem) {
   console.log(account.posts);
 });
 
-// account.posts.forEach(function (postItem) {
-//   const postEl = document.createElement("li");
-//   // prettier-ignore
-//   postEl.innerHTML = `
-//   <div class="account-posting">
-//               <img class="img-post" src="/profile.jpg" alt="profile" />
-//               <div class="info-post">
-//                 <p class="account">${postItem.name}</p>
-//                 <p class="days">${postItem.postDate}</p>
-//               </div>
-//             </div>
+account.posts.forEach(function (postItem) {
+  const postEl = document.createElement("li");
+  // prettier-ignore
+  postEl.innerHTML = `
+  <div class="account-posting">
+              <img class="img-post" src="/profile.jpg" alt="profile" />
+              <div class="info-post">
+                <p class="account">${postItem.name}</p>
+                <p class="days">${postItem.postDate}</p>
+              </div>
+            </div>
 
-//             <p class="post-text">
-//               Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit
-//               excepturi molestias ut fuga distinctio?
-//             </p>
+            <p class="post-text">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit
+              excepturi molestias ut fuga distinctio?
+            </p>
 
-//             <div class="likes-container">
-//               <ion-icon class="like-icon" name="thumbs-up-outline"></ion-icon>
-//               <!-- prettier-ignore -->
-//               <p class="like-text">  <span class="number-of-likes">2</span>&nbsp;&nbsp;&nbsp; Nina Vuksanovic and Angelina Simonovska</p>
-//             </div>
+            <div class="likes-container">
+              <ion-icon class="like-icon" name="thumbs-up-outline"></ion-icon>
+              <!-- prettier-ignore -->
+              <p class="like-text">  <span class="number-of-likes">2</span>&nbsp;&nbsp;&nbsp; Nina Vuksanovic and Angelina Simonovska</p>
+            </div>
 
-//             <div class="like-and-comment">
-//               <button class="like-btn">Like</button>
-//               <button class="comment-btn">Comment</button>
-//             </div>
+            <div class="like-and-comment">
+              <button class="like-btn">Like</button>
+              <button class="comment-btn">Comment</button>
+            </div>
 
-//             <div class="write-comment">
-//               <img class="comment-img" src="/profile.jpg" alt="profile" />
-//               <input type="text" class="input-comment" placeholder="Write a comment" />
-//             </div>
+            <div class="write-comment">
+              <img class="comment-img" src="/profile.jpg" alt="profile" />
+              <input type="text" class="input-comment" placeholder="Write a comment" />
+            </div>
 
-//             <ul class="comments">
-//             </ul>
-//           </li>
-//   `;
-//   postEl.className = "post-item";
-//   postEl.setAttribute("data-id", postItem.id);
-//   postListEl.appendChild(postEl);
-// });
+            <ul class="comments">
+            
+            </ul>
+          </li>
+  `;
+  postEl.className = "post-item";
+  postEl.setAttribute("data-id", postItem.id);
+  postListEl.appendChild(postEl);
+});
 
 class Like {
   constructor() {
@@ -147,5 +156,6 @@ class Comment {
     this.person = "";
     this.commentText = "";
     this.id = crypto.randomUUID();
+    this.photo = "";
   }
 }
